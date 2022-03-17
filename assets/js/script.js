b7c6af7591c8144633eaae6140a41cc4fe565841
detronetdip <ayondip2001@gmail.com>
2022-03-17 18:05:11 +0530

    Counter added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("Welcome");
+var countDownDate = new Date("May 23, 2022 00:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("days").innerHTML =days;
+  document.getElementById("hours").innerHTML =hours;
+  document.getElementById("mins").innerHTML =minutes;
+  document.getElementById("secs").innerHTML =seconds;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
