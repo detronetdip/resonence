5cb4eafef2eebb7bfa9d7ac7deb5ffc88d35d9bd
SukhenduBhukta <sukhendu.bhukta789@gmail.com>
2022-03-28 16:02:30 +0530

    glare

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,24 @@ var x = setInterval(function () {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("days").innerHTML =days;
-  document.getElementById("hours").innerHTML =hours;
-  document.getElementById("mins").innerHTML =minutes;
-  document.getElementById("secs").innerHTML =seconds;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("secs").innerHTML = seconds;
 }, 1000);
 var swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
-  loop:true,
+  loop: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
-  }, 
+  },
 });
 VanillaTilt.init(document.querySelectorAll(".scardcontainer"), {
-  max: 25,
-  speed: 400
+  max: 15,
+  speed: 400,
+  glare: true,
+  maxGlare: .5
 });
 //alert(screen.width + "*" + screen.height)
