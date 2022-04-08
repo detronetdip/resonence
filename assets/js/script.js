ecd700dbb2f777c06c38eded20ac76acf1241efd
SukhenduBhukta <sukhendu.bhukta789@gmail.com>
2022-04-08 17:10:39 +0530

    add carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,27 @@ VanillaTilt.init(document.querySelectorAll(".scardcontainer"), {
   "max-glare": 0.3,
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
