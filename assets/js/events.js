581931c75436aff63bd5a255d124c1e95f85210c
detronetdip <ayondip2001@gmail.com>
2022-04-08 22:18:16 +0530

    js added

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,15 +1,31 @@
-var allId = ["pkbt","aqua","rbrce","rbrce"];
-var parkobot = document.getElementById("pkbt");
-parkobot.style.height = 0;
+var allId = ["pkbt", "aqua", "rbrce", "rbrce", "obs"];
 function expand(e) {
-  var id = e.getAttribute("refer");
-  var isOpen = e.getAttribute("isOpen");
-  console.log(isOpen);
-  if (isOpen == "false") {
-    document.getElementById(id).style.height = "auto";
-    e.setAttribute("isOpen", true);
-  } else {
-    document.getElementById(id).style.height = "0";
-    e.setAttribute("isOpen", false);
-  }
+  // var id = e.getAttribute("refer");
+  // var isOpen = e.getAttribute("isOpen");
+  // console.log(isOpen);
+  // if (isOpen == "false") {
+  //   document.getElementById(id).style.height = "auto";
+  //   e.setAttribute("isOpen", true);
+  // } else {
+  //   document.getElementById(id).style.height = "0";
+  //   e.setAttribute("isOpen", false);
+  // }
+  allId.forEach((r) => {
+    if (e.nextSibling.nextSibling.id == r) {
+      var isOpen = e.getAttribute("isOpen");
+      if(isOpen=="false"){
+        e.nextSibling.nextSibling.style.height = "auto";
+        e.setAttribute("isOpen", true);
+      }else{
+        e.nextSibling.nextSibling.style.height = 0;
+        e.setAttribute("isOpen", false);
+      }
+    } else {
+      document.getElementById(r).style.height = 0;
+      document.getElementById(r).previousSibling.previousSibling.setAttribute("isOpen", false);
+    }
+  });
 }
+allId.forEach((e) => {
+  document.getElementById(e).style.height = 0;
+});
