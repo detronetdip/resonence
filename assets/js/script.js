f5f1b19bb7caf2725bb4c29e87bc8216615b0ada
detronetdip <ayondip2001@gmail.com>
2022-04-10 06:48:03 +0530

    slider changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,27 +28,14 @@ VanillaTilt.init(document.querySelectorAll(".scardcontainer"), {
   "max-glare": 0.3,
 });
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000);
-}
+var swiper = new Swiper(".mySwiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop:true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
