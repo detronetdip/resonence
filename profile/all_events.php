838f932d7bc778616275970037955008bccb64cc
SukhenduBhukta <sukhendu.bhukta789@gmail.com>
2022-04-12 22:56:29 +0530

    g

--- profile/all_events.php
+++ profile/all_events.php
@@ -11,24 +11,53 @@
                 <thead>
                   <th>#</th>
                   <th>Event Name</th>
-                  <th>Date</th>
-                  <th>Time</th>
+                  <th>Category</th>
+                  <th>Entry Fee</th>
                   <th>View</th>
                 </thead>
                 <tbody>
+                <?php 
+                $sql="SELECT sevname,price,evname FROM sevents a,events b WHERE a.mevent=b.id";
+                $res=mysqli_query($con,$sql);
+		            $product_arr=array();
+                while($row=mysqli_fetch_assoc($res)){	
+                    $product_arr[]=$row;
+                }
+                $i=1;
+                // prx($row);
+                foreach($product_arr as $r){
+                  ?>
                   <tr>
-                    <td>1</td>
-                    <td>Robowar</td>
-                    <td>12/12/12</td>
-                    <td>12:00:00</td>
+                    <td>
+                      <?php echo $i; $i++; ?>
+                    </td>
+                    <td>
+                    <?php echo $r['sevname']; ?>
+                    </td>
+                    <td>
+                    <?php echo $r['evname']; ?>
+                    </td>
+                    <td>
+                    &#8377; <?php echo $r['price']; ?>
+                    </td>
                     <td>
                       <div class="acn">
-                        <a href="reg-detail.html" class="view">
+                        <?php
+                        
+                        
+                        
+                        ?>
+                        <a href="../" class="view">
                           <i class="uil uil-eye"></i>
                         </a>
                       </div>
                     </td>
                   </tr>
+                  <?php 
+                
+                }
+                
+                ?>
                 </tbody>
               </table>
             </div>
