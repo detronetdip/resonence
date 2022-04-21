fddc6e3b6e0101c17237271a1b3656ac38fc8ead
detronetdip <ayondip2001@gmail.com>
2022-04-22 03:45:00 +0530

    final test

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,7 @@ element.addEventListener('submit', e => {
   }
 });
 function forgetPassword(){
+  control.html('button',"wait...");
   var mainEvent = control.getInput("fgtemail");
   $.ajax({
     url: HOST + "/backend/auth/forget.php",
@@ -111,7 +112,19 @@ function forgetPassword(){
     data: "mainEvent=" + mainEvent,
     success: function (htl) {
       var html = JSON.parse(htl);
-      console.log(html);
+      control.html('button',"wait...");
+      forgetMail(html.mail,html.name,html.link);
+    },
+  });
+}
+function forgetMail(mail,name,link){
+  control.html('button',"Sending mail....");
+  $.ajax({
+    url: HOST + "/backend/mail/fgt.php",
+    type: "post",
+    data: "mail=" + mail +"&name="+name+"&link="+link,
+    success: function (htl) {
+      swal("Mail sent")
     },
   });
 }
--- backend/auth/forget.php
+++ backend/auth/forget.php
@@ -1,13 +1,12 @@
 <?php
-  include('../smtp/PHPMailerAutoload.php');
   require('../../utility/utility.php');
-  require('../../mail_template/template.php');
   $email=get_safe_value($con,$_POST['mainEvent']);
   $result=array();
   $result['got']=$email;
   $q="SELECT * FROM users WHERE uemail='$email'";
   $res=mysqli_query($con,$q);
   $n=mysqli_num_rows($res);
+  $ni=mysqli_fetch_assoc($res);
   if($n=0){
     $result['status']=0;
     $result['msg']="No account is registered with this email id";
@@ -15,34 +14,8 @@
     $code=get_code();
     $_SESSION['FGT_CU_STORE']=$code;
     $result['link']="https://cemkfest.in/backend/auth/change_password/index.php?i=".$code."&ip=".password_hash($code, PASSWORD_DEFAULT);
+    $result['name']=$ni['uname'];
+    $result['mail']=$email;
   }
-  function smtp_mailer($to,$subject, $msg){
-    $mail = new PHPMailer(); 
-    $mail->SMTPDebug  = 3;
-    $mail->IsSMTP(); 
-    $mail->SMTPAuth = true; 
-    $mail->SMTPSecure = 'tls'; 
-    $mail->Host = "smtp.hostinger.com";
-    $mail->Port = 587; 
-    $mail->IsHTML(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->Username = "support@cemkfest.in";
-    $mail->Password = "12345678QWERTYUI!@#$%^&*qwertyui";
-    $mail->SetFrom("support@cemkfest.in");
-    $mail->Subject = $subject;
-    $mail->Body =$msg;
-    $mail->AddAddress($to);
-    $mail->SMTPOptions=array('ssl'=>array(
-      'verify_peer'=>false,
-      'verify_peer_name'=>false,
-      'allow_self_signed'=>false
-    ));
-    if(!$mail->Send()){
-      echo $mail->ErrorInfo;
-    }else{
-      return 'Sent';
-    }
-  }
-  smtp_mailer($mailID,'Forget Password',getFGTTemplate($name,$result['link']));
    echo json_encode($result);
 ?>
