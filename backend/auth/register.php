bd6a1f292505598f573effed8a29c63c8f9fedd7
detronetdip <ayondip2001@gmail.com>
2022-04-21 23:15:11 +0530

    s

--- backend/auth/register.php
+++ backend/auth/register.php
@@ -20,6 +20,8 @@
     $result['msg']="Enter Mobile";
   } else {
     $query="SELECT * FROM users WHERE uemail='$email'";
+    echo $query;
+    die();
     $res=mysqli_query($con,$query);
     $n=mysqli_num_rows($res);
     if($n>0){
