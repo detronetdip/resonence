b1cc113b8507a940a39be554500a6d127e44e287
detronetdip <ayondip2001@gmail.com>
2022-04-24 03:13:56 +0530

    cache clear

--- require/eventTop.php
+++ require/eventTop.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ResonanceXI</title>
-    <link rel="stylesheet" href="assets/css/events.css" />
+    <link rel="stylesheet" href="assets/css/events1.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
