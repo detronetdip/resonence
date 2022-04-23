3c2cec19d0b174b109f8e48e39d386313cd504c8
detronetdip <ayondip2001@gmail.com>
2022-04-23 23:38:52 +0530

    test2

--- backend/auth/change_password/index.php
+++ backend/auth/change_password/index.php
@@ -1,11 +1,13 @@
 <?php
   require('../../../utility/utility.php');
-    $precode=$_SESSION['FGT_CU_STORE'];
+    $uid=$_GET['ud'];
+    $udetail=mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM fgtp WHERE u_id='$uid'"));
+    $precode=$udetail['fcode'];
     $i=$_GET['i'];
     $ip=$_GET['ip'];
     $result=array();
     if($i==$precode){
-        unset($_SESSION['FGT_CU_STORE']);
+        mysqli_query($con,"DELETE FROM fgtp WHERE u_id='$uid'");
         $verify = password_verify($i, $ip);
         if($verify){
             echo "DONE";
