d5658ca57d93aef9ed8a373abc50254bc25171d9
detronetdip <ayondip2001@gmail.com>
2022-04-24 23:09:20 +0530

    h

--- entertainment.php
+++ entertainment.php
@@ -293,10 +293,10 @@
                     <li>A photo without meta-data will be disqualified i.e The photo should contain its date of creation, device name with which the photo was clicked and the other details in the meta-data.
                     </li>
                     <li>Photos that portray or otherwise include inappropriate and/or offensive content, including provocative nudity, violence, human rights and/or environmental violation, and/or any other contents deemed to be contrary to the law, religious, cultural & moral traditions and practices of India, are strictly prohibited and will be immediately discarded.</li>
-                    <li>The photo submissions of the photohike and fotolumi will be combined and evaluated.The photo which will be the best among the combined submissions (both photohike and fotolumi) will be awarded the first prize.In a similar way the second and the third prize will be awarded.
+                    <li>The photo submissions of the photohike and fotolumi will be combined and evaluated.The photo which will be the best among the combined submissions (both photohike and fotolumi) will be awarded the first prize.In a similar way the second prize will be awarded to the deserving participants.
                     </li>
                     <li>
-                      Note: There will be no separate award for each of the individual events. One single first, second and third prize will be awarded for both the events combined.
+                      Note: There will be no separate award for each of the individual events. One single first and second prize will be awarded for both the events combined (for photohike and fotolumi).
                       <ul>
                         <li>Final decision will be taken by our Judge.
                         </li>
