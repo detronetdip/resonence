5aa2dc8c4f11c5513a84863e9e5c0aea17f6cb20
detronetdip <ayondip2001@gmail.com>
2022-04-24 22:49:26 +0530

    l

--- entertainment.php
+++ entertainment.php
@@ -152,7 +152,7 @@
                 <!-- <span class="highlight2 pt5 p1">TASK</span>
               <p class="pt1 fs14">
                 Design a robot either wired or wireless within the specified dimensions that can operated manually
-          and can travel through all turns of the track .The fastest and steady robot will be the winner.
+                            and can travel through all turns of the track .The fastest and steady robot will be the winner.
 
                         </p> 
                         
@@ -269,14 +269,14 @@
                 <ol>
                   <li>PHOTOHIKE (THEME: COLLEGE CAMPUS)</li>
                     <li>FOTOLUMI (THEME: OPEN THEME)</li>
-                    <li>PHOTO EXHIBITION</li>
+                    <li>PHOTO ECHELEON (THE PHOTO EXHIBITION)</li>
                 </ol>
                 </p>
                 
                 <span class="highlight2 pt5 p1">RULES AND REGULATION</span>
                 <p class="pt1 fs14">
                   <ol style="margin-top: 0; padding-top: 0;">
-                    <li>The photo must be clicked within a given time frame. 
+                    <li>The photo must be clicked within a given time frame i.e. from 12:00 AM on 25/04/2022. 
                     </li>
                     <li>
                       The photo submitted must be the original work of the participant. By participating you are stating that you are the owner and the copyright holder of the photographs submitted.
@@ -288,8 +288,6 @@
                       Minor digital enhancements with brightness, contrast, saturation, hue can be made i.e Global editing is allowed but local editing is not allowed.
                     </li>
                     <li>A photo without meta-data will be disqualified i.e The photo should contain its date of creation, device name with which the photo was clicked and the other details in the meta-data.
-                    </li>
-                    <li>Only the photos submitted through google form will be accepted for the competition.
                     </li>
                     <li>Photos that portray or otherwise include inappropriate and/or offensive content, including provocative nudity, violence, human rights and/or environmental violation, and/or any other contents deemed to be contrary to the law, religious, cultural & moral traditions and practices of India, are strictly prohibited and will be immediately discarded.</li>
                     <li>The photo submissions of the photohike and fotolumi will be combined and evaluated.The photo which will be the best among the combined submissions (both photohike and fotolumi) will be awarded the first prize.In a similar way the second and the third prize will be awarded.
