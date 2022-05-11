fc685df195433fb2d08100882d26b37f9d3d430f
detronetdip <ayondip2001@gmail.com>
2022-05-11 09:36:47 +0530

    ghrf

--- try.php
+++ try.php
@@ -1,5 +1,5 @@
 <?php
-  //require('./utility/utility.php');
+  require('utility/utility.php');
   $q="SELECT * FROM `bb_d` WHERE name NOT IN ('DEBAKSHI BHATTACHARJEE','RAKESH RANA','MEHRIA NAWAZ','UMESH KUMAR SHAH','ARUP JANA','DEEP KUMAR ADAK','AMANDEEP REWANI','PRADIPTA GHOSHAL','SHIVAM KUMAR','SOVIK GHOSH','AMIT MANNA','MAYANK KUMAR','SANGLAP DUTTA','SOHAM PAUL','PRIYANGSU KOLEY','ADITYA DEEPAK','MUKUTMANI DAS');";
   $rs=mysql_query($con,$q);
   $d=array();
