b3eedccc3727c3d24ee13c1c113cf41f66f7f5a7
detronetdip <ayondip2001@gmail.com>
2022-05-11 09:37:58 +0530

    tryh

--- try.php
+++ try.php
@@ -1,5 +1,8 @@
 <?php
-  require('utility/utility.php');
+  session_start();
+  $con= mysqli_connect('localhost','u364601224_rootxiresonanc','K4=bD=Nb[p');
+mysqli_select_db($con,'u364601224_resonancexi');
+
   $q="SELECT * FROM `bb_d` WHERE name NOT IN ('DEBAKSHI BHATTACHARJEE','RAKESH RANA','MEHRIA NAWAZ','UMESH KUMAR SHAH','ARUP JANA','DEEP KUMAR ADAK','AMANDEEP REWANI','PRADIPTA GHOSHAL','SHIVAM KUMAR','SOVIK GHOSH','AMIT MANNA','MAYANK KUMAR','SANGLAP DUTTA','SOHAM PAUL','PRIYANGSU KOLEY','ADITYA DEEPAK','MUKUTMANI DAS');";
   $rs=mysql_query($con,$q);
   $d=array();
