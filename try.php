234bcedf3ee0e6376d1159a01278e43ab65bdecd
detronetdip <ayondip2001@gmail.com>
2022-05-11 09:34:51 +0530

    etryhr

--- try.php
+++ try.php
@@ -1,11 +1,11 @@
 <?php
   require('./utility/utility.php');
-//   $q="SELECT * FROM `bb_d` WHERE name NOT IN ('DEBAKSHI BHATTACHARJEE','RAKESH RANA','MEHRIA NAWAZ','UMESH KUMAR SHAH','ARUP JANA','DEEP KUMAR ADAK','AMANDEEP REWANI','PRADIPTA GHOSHAL','SHIVAM KUMAR','SOVIK GHOSH','AMIT MANNA','MAYANK KUMAR','SANGLAP DUTTA','SOHAM PAUL','PRIYANGSU KOLEY','ADITYA DEEPAK','MUKUTMANI DAS');";
-//   $rs=mysql_query($con,$q);
-//   $d=array();
-//   while($rw=mysqli_fetch_assoc($rs)){
-//     echo $rw['email']."\n";
-//   }
-echo "ok"
+  $q="SELECT * FROM `bb_d` WHERE name NOT IN ('DEBAKSHI BHATTACHARJEE','RAKESH RANA','MEHRIA NAWAZ','UMESH KUMAR SHAH','ARUP JANA','DEEP KUMAR ADAK','AMANDEEP REWANI','PRADIPTA GHOSHAL','SHIVAM KUMAR','SOVIK GHOSH','AMIT MANNA','MAYANK KUMAR','SANGLAP DUTTA','SOHAM PAUL','PRIYANGSU KOLEY','ADITYA DEEPAK','MUKUTMANI DAS');";
+  $rs=mysql_query($con,$q);
+  $d=array();
+  while($rw=mysqli_fetch_assoc($rs)){
+    echo $rw['email']."\n";
+  }
+echo "otk"
 
 ?>
